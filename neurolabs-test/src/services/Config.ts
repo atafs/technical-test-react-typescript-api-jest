1608bf3ea4419f6c156fc3a1521605e07914b61d
Americo Tomas <americotomaswork@gmail.com>
2025-06-03 14:27:04 +0100

    update api layer to read url from .env file too

--- neurolabs-test/src/services/Config.ts
+++ neurolabs-test/src/services/Config.ts
@@ -1,12 +1,13 @@
-export const API_BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "/v2"
-    : "https://staging.api.neurolabs.ai/v2";
+export const API_BASE_URL: string = process.env.REACT_APP_API_BASE_URL || "";
 
 export const API_KEY: string = process.env.REACT_APP_API_KEY || "";
 
 if (!API_KEY) {
   throw new Error("REACT_APP_API_KEY is not defined in the .env file");
+}
+
+if (!API_BASE_URL) {
+  throw new Error("REACT_APP_API_BASE_URL is not defined in the .env file");
 }
 
 // Control logging based on environment
